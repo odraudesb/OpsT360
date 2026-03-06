da358592fbbab7c6fa6c6843cfb7fa404b3a6643
odraudesb <66796718+odraudesb@users.noreply.github.com>
2026-03-05 19:20:19 -0500

    Fix seal command parameter type crash on navigation

---
+++ OpsT360/MauiProgram.cs
@@ -0,0 +1,41 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using OpsT360.Services;
+using OpsT360.ViewModels;
+using OpsT360.Views;
+
+namespace OpsT360;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+        builder.Services.AddSingleton<IAuthState, AuthState>();
+        builder.Services.AddTransient<AuthHeaderHandler>();
+
+        builder.Services.AddHttpClient<ILoginService, LoginService>();
+
+        builder.Services.AddHttpClient<ITransactionsService, TransactionsService>()
+            .AddHttpMessageHandler<AuthHeaderHandler>();
+
+        builder.Services.AddSingleton<LoginViewModel>();
+        builder.Services.AddSingleton<SealInspectionViewModel>();
+        builder.Services.AddSingleton<LoginPage>();
+        builder.Services.AddSingleton<SealInspectionPage>();
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+
+        return builder.Build();
+    }
+}
